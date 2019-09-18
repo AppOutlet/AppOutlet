6e3e970a3f822b85dc0fc3f1aa9a479980020184
Messias Junior <messiaslima.03@gmail.com>
2019-09-18 18:01:53 +0100

    WIP: Search by name

--- src/app/presentation/main/main.component.ts
+++ src/app/presentation/main/main.component.ts
@@ -21,7 +21,11 @@ export class MainComponent implements OnInit {
         this.presenter.init(this)
     }
 
-    categoryClicked(category: Category){
+    categoryClicked(category: Category) {
         this.presenter.categoryClicked(category)
+    }
+
+    search(query) {
+        console.log(query)
     }
 }
