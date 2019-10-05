0d4f42e8f6729a49a415d6f19942deaf7fa14081
Messias Junior <messiaslima.03@gmail.com>
2019-10-05 03:08:25 +0100

    feat: about page

--- src/app/presentation/settings/about/about.component.ts
+++ src/app/presentation/settings/about/about.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { version, authors } from './../../../../../package.json';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    appVersion: string
+    authors = []
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.appVersion = version
+        this.authors = authors
+    }
 
+    ngOnInit() {
+    }
 }
