e61372a0bdae6bf7e5a0de1fe588424624861398
Messias Junior <messiaslima.03@gmail.com>
2020-01-21 18:01:14 +0000

    refactor: removing some ui mode logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import 'reflect-metadata';
-import '../polyfills';
+import 'reflect-metadata'
+import '../polyfills'
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { IonicModule } from '@ionic/angular';
-import { AppRoutingModule } from './app-routing.module';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { AppComponent } from './app.component';
-import { MainModule } from './presentation/main/main.module';
-import { StoreSetupComponent } from './presentation/settings/store-setup/store-setup.component';
-import { CacheInterceptor } from './interceptor/cache.interceptor';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { CoreModule } from './core/core.module'
+import { SharedModule } from './shared/shared.module'
+import { IonicModule } from '@ionic/angular'
+import { AppRoutingModule } from './app-routing.module'
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { AppComponent } from './app.component'
+import { MainModule } from './presentation/main/main.module'
+import { StoreSetupComponent } from './presentation/settings/store-setup/store-setup.component'
+import { CacheInterceptor } from './interceptor/cache.interceptor'
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json')
 }
 
 @NgModule({
@@ -29,9 +29,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         CoreModule,
         SharedModule,
         MainModule,
-        IonicModule.forRoot({
-            mode: (window as any).uiMode || 'ios'
-        }),
+        IonicModule.forRoot(),
         AppRoutingModule,
         TranslateModule.forRoot({
             loader: {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const AppConfig = {
     production: true,
     environment: 'PROD',
     baseUrl: 'https://appoutlet.herokuapp.com'
-};
+}
