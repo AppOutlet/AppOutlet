47bf1211ce4f0ba070e94a3f3e867c7e086c2133
Messias Junior <messiaslima.03@gmail.com>
2020-01-27 18:26:58 +0000

    feature(Get involved): 'Reporting bugs' section

--- src/app/presentation/get-involved/get-involved.component.ts
+++ src/app/presentation/get-involved/get-involved.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ElectronService } from '../../core/services'
+import { ToastController } from '@ionic/angular'
 
 @Component({
-  selector: 'app-get-involved',
-  templateUrl: './get-involved.component.html',
-  styleUrls: ['./get-involved.component.scss']
+    selector: 'app-get-involved',
+    templateUrl: './get-involved.component.html',
+    styleUrls: ['./get-involved.component.scss']
 })
 export class GetInvolvedComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private electronService: ElectronService,
+        private toastController: ToastController
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    openLink(url: string) {
+        this.electronService.shell.openExternal(url)
+        this.showMessageToast()
+    }
+
+    async showMessageToast() {
+        const toast = await this.toastController.create({
+            header: 'External resource',
+            message: 'The page will open in you browser',
+            buttons: [{
+                text: 'OK',
+                role: 'cancel'
+            }]
+        })
+        toast.present()
+    }
 }
