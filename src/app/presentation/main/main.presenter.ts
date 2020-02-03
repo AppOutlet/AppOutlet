d3bda70381477aaf52778bc9b4ab635461de5156
Messias Junior <messiaslima.03@gmail.com>
2020-02-03 20:03:43 +0000

    chore: removing snap build and deploy

--- src/app/presentation/main/main.presenter.ts
+++ src/app/presentation/main/main.presenter.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core'
-import { MainRouter } from './main.router'
-import { MainComponent } from './main.component'
-import { TagService } from '../../core/services/tag/tag.service'
-import { Tag } from '../../core/model/tag.model'
-import { SearchType } from '../../core/model/search-type'
-import { EventBusService } from 'ngx-eventbus'
-import { CategoryService } from '../../core/services/category/category.service'
-import { Category } from '../../core/model/category.model'
+import { Injectable } from '@angular/core';
+import { MainRouter } from './main.router';
+import { MainComponent } from './main.component';
+import { TagService } from '../../core/services/tag/tag.service';
+import { Tag } from '../../core/model/tag.model';
+import { SearchType } from '../../core/model/search-type';
+import { EventBusService } from 'ngx-eventbus';
+import { CategoryService } from '../../core/services/category/category.service';
+import { Category } from '../../core/model/category.model';
 
 @Injectable()
 export class MainPresenter {
 
-    private view: MainComponent
+    private view: MainComponent;
 
     constructor(
         private router: MainRouter,
@@ -21,36 +21,36 @@ export class MainPresenter {
     ) { }
 
     init(view: MainComponent) {
-        this.view = view
-        this.getAllCategories()
+        this.view = view;
+        this.getAllCategories();
     }
 
     getAllCategories() {
         this.categoryService.getAll().subscribe(categoryList => {
-            this.view.categoryList = categoryList
-        })
+            this.view.categoryList = categoryList;
+        });
     }
 
     tagClicked(tag: Tag) {
-        this.tagService.setSelectedTag(tag)
-        this.eventBusService.triggerEvent('tagSelected', tag)
-        this.router.goToSearchResult(SearchType.TAG)
+        this.tagService.setSelectedTag(tag);
+        this.eventBusService.triggerEvent('tagSelected', tag);
+        this.router.goToSearchResult(SearchType.TAG);
     }
 
     categoryClicked(category: Category) {
-        this.categoryService.setSelectedCategory(category)
-        this.eventBusService.triggerEvent('categorySelected', category)
-        this.router.goToSearchResult(SearchType.CATEGORY)
+        this.categoryService.setSelectedCategory(category);
+        this.eventBusService.triggerEvent('categorySelected', category);
+        this.router.goToSearchResult(SearchType.CATEGORY);
     }
 
     search(query: string) {
         if (this.isQueryValid(query)) {
-            this.eventBusService.triggerEvent('queryTyped', query)
-            this.router.goToSearchResult(SearchType.NAME, query)
+            this.eventBusService.triggerEvent('queryTyped', query);
+            this.router.goToSearchResult(SearchType.NAME, query);
         }
     }
 
     private isQueryValid(query: string): boolean {
-        return query != null && query.length > 2
+        return query != null && query.length > 2;
     }
 }
