c48e8b5dafbabc656afb0a05cb231d51976960b8
Messias Junior <messiaslima.03@gmail.com>
2021-02-17 20:27:55 +0000

    feat: create snap store repo

---
+++ core/repository/snapstore/SnapStoreRepository.spec.js
@@ -0,0 +1,39 @@
+describe('Snap store repository', () => {
+    const mockedApps = [
+        {
+            private: false,
+            publisher: 'Graham Morrison',
+            ratings_average: 0.0,
+            release: ['16'],
+            revision: 59,
+            snap_id: 'lVMsb0E4Z52tx3zny2OKsAR07Gh6ZyzO',
+            summary: 'Storage utilisation viewer',
+            support_url: '',
+            title: 'qFSView',
+            version: 'a313751',
+            website: 'https://github.com/blastrock/qfsview',
+        },
+    ];
+
+    const mockResponse = {
+        data: {
+            _embedded: {
+                'clickindex:package': mockedApps,
+            },
+        },
+    };
+
+    const mockAxios = {
+        get: () => Promise.resolve(mockResponse),
+    };
+
+    jest.mock('axios', () => mockAxios);
+
+    const snapStoreRepository = require('./SnapStoreRepository');
+
+    it('Should get all apps', async () => {
+        const apps = await snapStoreRepository.getApps();
+
+        expect(apps).toEqual(mockedApps);
+    });
+});
