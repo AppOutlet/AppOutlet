f6890755e1ec15cad4003c68ad9bdd887f89fbd2
Messias Junior <messiaslima.03@gmail.com>
2021-02-23 21:20:43 +0000

    feat: translate category menu buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule } from '@nebular/theme';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NgxElectronModule } from 'ngx-electron';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxElectronModule } from 'ngx-electron';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule } from '@nebular/theme';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
     declarations: [AppComponent],
@@ -14,7 +21,16 @@ import { NbThemeModule } from '@nebular/theme';
         AppRoutingModule,
         NgxElectronModule,
         BrowserAnimationsModule,
+        HttpClientModule,
         NbThemeModule.forRoot({ name: 'default' }),
+        TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent],
