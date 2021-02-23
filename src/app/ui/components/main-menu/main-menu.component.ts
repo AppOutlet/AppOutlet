1fb363296b5bc18b3aa9ca00a946524bbb1745cb
Messias Junior <messiaslima.03@gmail.com>
2021-02-23 21:20:43 +0000

    feat: translate category menu buttons

--- src/app/ui/components/main-menu/main-menu.component.ts
+++ src/app/ui/components/main-menu/main-menu.component.ts
@@ -1,56 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NbMenuItem } from '@nebular/theme';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-main-menu',
     templateUrl: './main-menu.component.html',
     styleUrls: ['./main-menu.component.scss'],
 })
-export class MainMenuComponent {
-    items: NbMenuItem[] = [
-        {
-            title: 'Office',
-            icon: 'layers-outline',
-        },
-        {
-            title: 'Internet and Communication',
-            icon: 'globe-2-outline',
-        },
-        {
-            title: 'Audio',
-            icon: { icon: 'music-outline' },
-        },
-        {
-            title: 'Video',
-            icon: 'video-outline',
-        },
-        {
-            title: 'Games',
-            icon: 'cube-outline',
-        },
-        {
-            title: 'Development',
-            icon: 'code-outline',
-        },
-        {
-            title: 'Finance',
-            icon: 'bar-chart-outline',
-        },
-        {
-            title: 'Graphics',
-            icon: 'image-outline',
-        },
-        {
-            title: 'Science and Education',
-            icon: 'book-outline',
-        },
-        {
-            title: 'Utility and Productivity',
-            icon: 'bulb-outline',
-        },
-        {
-            title: 'Miscellaneous',
-            icon: 'grid-outline',
-        },
-    ];
+export class MainMenuComponent implements OnInit {
+    constructor(private translationService: TranslateService) {}
+
+    items: NbMenuItem[] = [];
+
+    ngOnInit(): void {
+        this.setupMenuItems();
+    }
+
+    async setupMenuItems(): Promise<void> {
+        this.items = [
+            {
+                title: await this.getTranslation('PAGES.MAIN.CATEGORY.OFFICE'),
+                icon: 'layers-outline',
+            },
+            {
+                title: await this.getTranslation(
+                    'PAGES.MAIN.CATEGORY.INTERNET_COMMUNICATION',
+                ),
+                icon: 'globe-2-outline',
+            },
+            {
+                title: await this.getTranslation('PAGES.MAIN.CATEGORY.AUDIO'),
+                icon: { icon: 'music-outline' },
+            },
+            {
+                title: await this.getTranslation('PAGES.MAIN.CATEGORY.VIDEO'),
+                icon: 'video-outline',
+            },
+            {
+                title: await this.getTranslation('PAGES.MAIN.CATEGORY.GAMES'),
+                icon: 'cube-outline',
+            },
+            {
+                title: await this.getTranslation(
+                    'PAGES.MAIN.CATEGORY.DEVELOPMENT',
+                ),
+                icon: 'code-outline',
+            },
+            {
+                title: await this.getTranslation('PAGES.MAIN.CATEGORY.FINANCE'),
+                icon: 'bar-chart-outline',
+            },
+            {
+                title: await this.getTranslation(
+                    'PAGES.MAIN.CATEGORY.GRAPHICS',
+                ),
+                icon: 'image-outline',
+            },
+            {
+                title: await this.getTranslation(
+                    'PAGES.MAIN.CATEGORY.SCIENCE_EDUCATION',
+                ),
+                icon: 'book-outline',
+            },
+            {
+                title: await this.getTranslation(
+                    'PAGES.MAIN.CATEGORY.UTILITY_PRODUCTIVITY',
+                ),
+                icon: 'bulb-outline',
+            },
+            {
+                title: await this.getTranslation(
+                    'PAGES.MAIN.CATEGORY.MISCELLANEOUS',
+                ),
+                icon: 'grid-outline',
+            },
+        ];
+    }
+
+    private getTranslation(key: string): Promise<string> {
+        return this.translationService.get(key).toPromise();
+    }
 }
--- src/app/ui/components/main-menu/main-menu.module.ts
+++ src/app/ui/components/main-menu/main-menu.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainMenuComponent } from './main-menu.component';
 import { NbMenuModule } from '@nebular/theme';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [MainMenuComponent],
-    imports: [CommonModule, NbMenuModule.forRoot()],
+    imports: [CommonModule, NbMenuModule.forRoot(), TranslateModule.forChild()],
     exports: [MainMenuComponent],
 })
 export class MainMenuModule {}
