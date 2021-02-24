7300e2a87f944c8fe18f4fe02acc864a6a7b6cf9
Messias Junior <messias@pop-os.localdomain>
2021-02-24 22:03:47 +0000

    feat: section component

--- src/app/ui/components/card/card.component.spec.ts
+++ src/app/ui/components/card/card.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NbCardModule } from '@nebular/theme';
 
 import { CardComponent } from './card.component';
+import DoneCallback = jest.DoneCallback;
+import { CardDto } from './card.dto';
 
 describe('CardComponent', () => {
     let component: CardComponent;
@@ -26,5 +28,31 @@ describe('CardComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should emit event when card is clicked', (done: DoneCallback) => {
+        const app: CardDto = {
+            id: '1',
+            icon: 'icon',
+            summary: 'summary',
+            title: 'title',
+        };
+
+        component.applicationClicked.subscribe((event) => {
+            expect(event).toEqual(app);
+            done();
+        });
+
+        component.click(app);
+    });
+
+    it('should not emit event when app is null', (done: DoneCallback) => {
+        component.applicationClicked.subscribe(() => {
+            fail();
+        });
+
+        component.click(undefined);
+
+        done();
     });
 });
--- src/app/ui/components/section/section.module.ts
+++ src/app/ui/components/section/section.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionComponent } from './section.component';
 import { CardModule } from '../card/card.module';
+import { NbButtonModule } from '@nebular/theme';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [SectionComponent],
     exports: [SectionComponent],
-    imports: [CommonModule, CardModule],
+    imports: [
+        CommonModule,
+        CardModule,
+        NbButtonModule,
+        TranslateModule.forChild(),
+    ],
 })
 export class SectionModule {}
