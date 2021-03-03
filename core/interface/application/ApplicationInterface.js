c456f84aa1113fc129bd2e3cab321025f0777af9
Messias Junior <messiaslima.03@gmail.com>
2021-03-03 20:04:13 +0000

    fix: listen to events if the core was on electron

--- core/interface/application/ApplicationInterface.js
+++ core/interface/application/ApplicationInterface.js
@@ -2,10 +2,10 @@ const { ipcMain } = require('electron');
 const channels = require('../InterfaceChannel');
 const applicationService = require('../../service/application/ApplicationService');
 
-ipcMain.handle(channels.application.getRecentlyAdded, () => {
+ipcMain?.handle(channels.application.getRecentlyAdded, () => {
     return applicationService.getRecentlyAdded();
 });
 
-ipcMain.handle(channels.application.getRecentlyUpdated, () => {
+ipcMain?.handle(channels.application.getRecentlyUpdated, () => {
     return applicationService.getRecentlyUpdated();
 });
