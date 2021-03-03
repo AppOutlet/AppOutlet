1403f3b40b7def7cfe379b0cea6173fa6b4dbbfa
Messias Junior <messiaslima.03@gmail.com>
2021-03-03 23:45:26 +0000

    test: unit tests for application related items

--- src/app/service/application/application.service.spec.ts
+++ src/app/service/application/application.service.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApplicationService } from './application.service';
+import { CoreService } from '../core/core.service';
+import { Application } from '../../model/application.model';
 
 describe('ApplicationService', () => {
     let service: ApplicationService;
 
+    const mockCoreService = {
+        invoke: jest.fn(),
+    };
+
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [{ provide: CoreService, useValue: mockCoreService }],
+        });
         service = TestBed.inject(ApplicationService);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('Should get recently added apps', async () => {
+        const mockedApps: Application[] = [{ id: '', name: '' }];
+
+        mockCoreService.invoke.mockReturnValue(Promise.resolve(mockedApps));
+
+        const result = await service.getRecentlyAdded();
+
+        expect(result).toEqual(mockedApps);
+    });
+
+    it('Should get recently updated apps', async () => {
+        const mockedApps: Application[] = [{ id: '', name: '' }];
+
+        mockCoreService.invoke.mockReturnValue(Promise.resolve(mockedApps));
+
+        const result = await service.getRecentlyUpdated();
+
+        expect(result).toEqual(mockedApps);
     });
 });
