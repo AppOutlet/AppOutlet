16a0f23cc6aa2e68a0d4f74f3951c24bc8d794a5
Messias Junior <messiaslima.03@gmail.com>
2021-03-08 19:30:57 +0000

    feat: navigate to application details when click on card

--- src/app/ui/pages/application/application.component.ts
+++ src/app/ui/pages/application/application.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-application',
-  templateUrl: './application.component.html',
-  styleUrls: ['./application.component.scss']
+    selector: 'app-application',
+    templateUrl: './application.component.html',
+    styleUrls: ['./application.component.scss'],
 })
-export class ApplicationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ApplicationComponent {}
