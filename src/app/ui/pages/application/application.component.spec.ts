04fea54e6482f93bc797f27501f7a5ad94fadd4f
Messias Junior <messiaslima.03@gmail.com>
2021-03-11 10:00:14 +0000

    test: fix tests for application component

--- src/app/ui/pages/application/application.component.spec.ts
+++ src/app/ui/pages/application/application.component.spec.ts
@@ -1,15 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApplicationComponent } from './application.component';
+import { of } from 'rxjs';
+import { Application } from '../../../model/application.model';
+import { ActivatedRoute } from '@angular/router';
+import { ApplicationService } from '../../../service/application/application.service';
+import { Location } from '@angular/common';
+import { CoreService } from '../../../service/core/core.service';
 
 describe('ApplicationComponent', () => {
     let component: ApplicationComponent;
     let fixture: ComponentFixture<ApplicationComponent>;
 
+    const mockActivatedRoute = {
+        paramMap: of({ get: (): string | null => 'category' }),
+    };
+
+    const mockApplicationService = {
+        findByCategory: (): Promise<Application[]> => Promise.resolve([]),
+    };
+
+    const mockLocation = {};
+
+    const mockCoreService = {};
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [ApplicationComponent],
-        }).compileComponents();
+            providers: [
+                { provide: ActivatedRoute, useValue: mockActivatedRoute },
+                {
+                    provide: ApplicationService,
+                    useValue: mockApplicationService,
+                },
+                { provide: Location, useValue: mockLocation },
+                { provide: CoreService, useValue: mockCoreService },
+            ],
+        })
+            .overrideComponent(ApplicationComponent, { set: { template: '' } })
+            .compileComponents();
     });
 
     beforeEach(() => {
