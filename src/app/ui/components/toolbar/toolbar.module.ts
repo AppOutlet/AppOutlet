924216289b2325e405fc28c4b436eafb6e763e60
Messias Junior <messiaslima.03@gmail.com>
2021-03-11 09:27:16 +0000

    feat: search button

--- src/app/ui/components/toolbar/toolbar.module.ts
+++ src/app/ui/components/toolbar/toolbar.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar.component';
-import { NbButtonModule, NbIconModule } from '@nebular/theme';
+import { NbButtonModule, NbIconModule, NbSearchModule } from '@nebular/theme';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [ToolbarComponent],
-    imports: [CommonModule, NbIconModule, NbButtonModule, TranslateModule],
+    imports: [
+        CommonModule,
+        NbIconModule,
+        NbButtonModule,
+        TranslateModule,
+        NbSearchModule,
+    ],
     exports: [ToolbarComponent],
 })
 export class ToolbarModule {}
