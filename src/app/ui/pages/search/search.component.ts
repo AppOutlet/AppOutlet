94ecac89b98af8cda1bea41cc514dabcde6de0cc
Messias Junior <messiaslima.03@gmail.com>
2021-03-11 19:48:52 +0000

    test: add tests for application component

--- src/app/ui/pages/search/search.component.ts
+++ src/app/ui/pages/search/search.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class SearchComponent {}
