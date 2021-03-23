b93e9d74703796edd341d7a7a5b7c7852761c570
Messias Junior <messiaslima.03@gmail.com>
2021-03-23 21:00:34 +0000

    feat: download button component

--- src/app/ui/components/install-button/install-button.component.ts
+++ src/app/ui/components/install-button/install-button.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    Output,
+    SimpleChanges,
+} from '@angular/core';
+import { ApplicationStatus } from '../../../model/application-status';
 
 @Component({
-  selector: 'app-install-button',
-  templateUrl: './install-button.component.html',
-  styleUrls: ['./install-button.component.scss']
+    selector: 'app-install-button',
+    templateUrl: './install-button.component.html',
+    styleUrls: ['./install-button.component.scss'],
 })
-export class InstallButtonComponent implements OnInit {
+export class InstallButtonComponent implements OnChanges {
+    @Input() applicationStatus?: ApplicationStatus;
+    @Input() installationPercentage = 0;
+    @Input() isIndefinite = true;
+    @Output() installClicked = new EventEmitter<void>();
+    @Output() uninstallClicked = new EventEmitter<void>();
 
-  constructor() { }
+    buttonStatus = 'basic';
+    buttonText = '';
+    buttonIcon = '';
+    buttonEnabled = true;
+    shouldShowButtonIcon = true;
+    loading = false;
 
-  ngOnInit(): void {
-  }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ngOnChanges(changes: SimpleChanges): void {
+        this.updateButtonAppearance();
+    }
 
+    private updateButtonAppearance(): void {
+        switch (this.applicationStatus) {
+            case ApplicationStatus.NOT_INSTALLED:
+                this.buttonStatus = 'success';
+                this.buttonText = 'PAGES.APP_DETAIL.INSTALL';
+                this.buttonIcon = 'download-outline';
+                this.buttonEnabled = true;
+                this.shouldShowButtonIcon = true;
+                this.loading = false;
+                break;
+
+            case ApplicationStatus.INSTALLED:
+                this.buttonStatus = 'danger';
+                this.buttonText = 'PAGES.APP_DETAIL.UNINSTALL';
+                this.buttonIcon = 'trash-outline';
+                this.buttonEnabled = true;
+                this.shouldShowButtonIcon = true;
+                this.loading = false;
+                break;
+
+            case ApplicationStatus.INSTALLING:
+                this.buttonText = 'PAGES.APP_DETAIL.INSTALLING';
+                this.buttonStatus = 'basic';
+                this.buttonEnabled = false;
+                this.shouldShowButtonIcon = false;
+                this.loading = true;
+                break;
+
+            default:
+                this.buttonStatus = 'basic';
+                this.buttonText = '';
+        }
+    }
 }
--- src/app/ui/components/install-button/install-button.module.ts
+++ src/app/ui/components/install-button/install-button.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbButtonModule } from '@nebular/theme';
+import {
+    NbButtonModule,
+    NbIconModule,
+    NbProgressBarModule,
+    NbSpinnerModule,
+} from '@nebular/theme';
 import { InstallButtonComponent } from './install-button.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     declarations: [InstallButtonComponent],
-    imports: [CommonModule, NbButtonModule],
+    imports: [
+        CommonModule,
+        NbButtonModule,
+        NbIconModule,
+        TranslateModule.forChild(),
+        NbSpinnerModule,
+        NbProgressBarModule,
+    ],
     exports: [InstallButtonComponent],
 })
 export class InstallButtonModule {}
