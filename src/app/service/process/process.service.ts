61bd0df99a35e57a2b1cf401b930a74ce0707398
Messias Junior <messiaslima.03@gmail.com>
2021-03-24 20:32:16 +0000

    feat: verify if snap is installed

--- src/app/service/process/process.service.ts
+++ src/app/service/process/process.service.ts
@@ -6,13 +6,14 @@ import { Process } from './process';
 import { InstallSnap } from './install-snap.process';
 import { ProcessQueue } from './ProcessQueue';
 import { ApplicationStatus } from '../../model/application-status';
+import { AppOutletChildProcess } from '../../util/app-outlet-child-process';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ProcessService {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    private readonly childProcess: any;
+    private readonly childProcess: AppOutletChildProcess;
     private readonly processQueue = new ProcessQueue();
 
     constructor(windowRef: WindowRef) {
@@ -54,17 +55,56 @@ export class ProcessService {
         this.processQueue.notifyProcessFinished(process);
     }
 
-    getApplicationStatus(application: Application): Promise<ApplicationStatus> {
+    async getApplicationStatus(
+        application: Application,
+    ): Promise<ApplicationStatus> {
         if (this.isApplicationInstalling(application)) {
             return Promise.resolve(ApplicationStatus.INSTALLING);
+        } else if (await this.isApplicationInstalled(application)) {
+            return Promise.resolve(ApplicationStatus.INSTALLED);
+        } else {
+            return Promise.resolve(ApplicationStatus.NOT_INSTALLED);
         }
-
-        return Promise.resolve(ApplicationStatus.NOT_INSTALLED);
     }
 
     private isApplicationInstalling(application: Application): boolean {
         return this.processQueue
             .getProcessList()
             .some((process) => process.applicationId === application.id);
+    }
+
+    private async isApplicationInstalled(
+        application: Application,
+    ): Promise<boolean> {
+        switch (application.packageType) {
+            case PackageType.SNAP:
+                return this.isSnapInstalled(application);
+            default:
+                return false;
+        }
+    }
+
+    private isSnapInstalled(application: Application): Promise<boolean> {
+        return this.executeCommand(
+            `snap list | grep ${application.packageName}`,
+        )
+            .then((output: string) => {
+                return output.length > 0;
+            })
+            .catch(() => {
+                return false;
+            });
+    }
+
+    private executeCommand(command: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.childProcess.exec(command, (error, stdout, stderr) => {
+                if (error || stderr) {
+                    reject(error || stderr);
+                } else {
+                    resolve(stdout);
+                }
+            });
+        });
     }
 }
