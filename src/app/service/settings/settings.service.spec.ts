fddc95d05f93372dc0a35007eceb0e960063da6a
Messias Junior <messiaslima.03@gmail.com>
2021-04-01 20:48:06 +0100

    test: fix app and settings tests

--- src/app/service/settings/settings.service.spec.ts
+++ src/app/service/settings/settings.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SettingsService } from './settings.service';
+import { CoreService } from '../core/core.service';
 
 describe('SettingsService', () => {
     let service: SettingsService;
+    const mockCoreService = {};
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [{ provide: CoreService, useValue: mockCoreService }],
+        });
         service = TestBed.inject(SettingsService);
     });
 
