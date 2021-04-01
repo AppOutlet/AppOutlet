73bd33b1389967803f3f00fc5e11cadc1f1d6e16
Messias Junior <messiaslima.03@gmail.com>
2021-04-01 20:38:23 +0100

    test: appearance component

--- src/app/ui/pages/settings/appearance/appearance.component.spec.ts
+++ src/app/ui/pages/settings/appearance/appearance.component.spec.ts
@@ -1,15 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppearanceComponent } from './appearance.component';
+import { NbThemeService } from '@nebular/theme';
+import { SettingsService } from '../../../../service/settings/settings.service';
 
 describe('AppearanceComponent', () => {
     let component: AppearanceComponent;
     let fixture: ComponentFixture<AppearanceComponent>;
 
+    const mockThemeService = { changeTheme: jest.fn() };
+    const mockSettingsService = { setTheme: jest.fn() };
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [AppearanceComponent],
-        }).compileComponents();
+            providers: [
+                { provide: NbThemeService, useValue: mockThemeService },
+                { provide: SettingsService, useValue: mockSettingsService },
+            ],
+        })
+            .overrideComponent(AppearanceComponent, { set: { template: '' } })
+            .compileComponents();
     });
 
     beforeEach(() => {
@@ -20,5 +31,25 @@ describe('AppearanceComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    // tslint:disable-next-line:quotemark
+    it("should show 'default' as default theme", () => {
+        expect(component.selectedTheme).toEqual('default');
+    });
+
+    it('should set theme when is successfully saved', async () => {
+        mockSettingsService.setTheme.mockReturnValue(Promise.resolve());
+        const selectedTheme = 'cosmic';
+        await component.onThemeChange(selectedTheme);
+
+        expect(mockSettingsService.setTheme.mock.calls.length).toBe(1);
+        expect(mockSettingsService.setTheme.mock.calls[0][0]).toBe(
+            selectedTheme,
+        );
+        expect(mockThemeService.changeTheme.mock.calls.length).toBe(1);
+        expect(mockThemeService.changeTheme.mock.calls[0][0]).toBe(
+            selectedTheme,
+        );
     });
 });
