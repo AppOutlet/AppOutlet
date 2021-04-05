1c99aed0d0e1dfab24eba5e5fdad1207c85c4565
Messias Junior <messiaslima.03@gmail.com>
2021-04-05 22:22:37 +0100

    refactor: make process more generic

--- src/app/service/process/snap/install-snap.process.ts
+++ src/app/service/process/snap/install-snap.process.ts
@@ -1,48 +1,19 @@
-import { Process, ProcessStatus } from '../process';
+import { Process } from '../process';
 import { Application } from '../../../model/application.model';
 import { AppOutletChildProcess } from '../../../util/app-outlet-child-process';
-import { ProcessInfo } from '../process-info';
 
 export class InstallSnap extends Process {
-    private processStatus = ProcessStatus.IDLE;
-
     constructor(
-        private childProcess: AppOutletChildProcess,
-        private application: Application,
-        private onProcessFinished: (process: Process) => void,
+        childProcess: AppOutletChildProcess,
+        application: Application,
+        private onProcessFinishedCallback: (process: Process) => void,
     ) {
-        super();
+        super(childProcess, application);
         this.isIndefinite = true;
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    close(): void {}
-
-    start(): void {
-        this.processStatus = ProcessStatus.RUNNING;
-
-        const command = this.generateInstallCommand();
-        const process = this.childProcess.spawn('pkexec', command);
-
-        process.stdout.on('data', (data: Buffer) => {
-            const output = data.toString();
-            this.processOutput(output);
-        });
-
-        process.on('error', (data: Buffer) => {
-            console.error(`ps stderr: ${data.toString()}`);
-            this.stderr.push(data.toString());
-        });
-
-        process.stderr.on('data', (data: Buffer) => {
-            console.error(`ps stderr: ${data.toString()}`);
-            this.stderr.push(data.toString());
-        });
-
-        process.on('close', () => {
-            this.processStatus = ProcessStatus.FINISHED;
-            this.onProcessFinished(this);
-        });
+    setupProcess(): void {
+        this.setCommand('pkexec', this.generateInstallCommand());
     }
 
     private generateInstallCommand(): string[] {
@@ -70,25 +41,7 @@ export class InstallSnap extends Process {
         }
     }
 
-    private processOutput(output: string): void {
-        this.stdout.push(output);
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    update(): void {}
-
-    getApplicationId(): string {
-        return this.application.id;
-    }
-
-    getProcessStatus(): ProcessStatus {
-        return this.processStatus;
-    }
-
-    getProcessInfo(): ProcessInfo {
-        return {
-            applicationId: this.getApplicationId(),
-            processStatus: this.getProcessStatus(),
-        };
+    onProcessFinished(process: Process): void {
+        this.onProcessFinishedCallback?.(process);
     }
 }
