3d7522f0096a5e8f288bc2c0a4233cd1df7ed04c
Messias Junior <messiaslima.03@gmail.com>
2021-04-05 22:22:37 +0100

    refactor: make process more generic

--- src/app/service/process/process.ts
+++ src/app/service/process/process.ts
@@ -1,23 +1,79 @@
 import { ProcessInfo } from './process-info';
+import { AppOutletChildProcess } from '../../util/app-outlet-child-process';
+import { Application } from '../../model/application.model';
 
 export abstract class Process {
+    private processStatus = ProcessStatus.IDLE;
+    private command?: string;
+    private arguments?: string[];
+
     protected stdout: string[] = [];
     protected stderr: string[] = [];
 
     isIndefinite = true;
     completePercentage = 0;
 
-    abstract start(): void;
+    protected constructor(
+        private childProcess: AppOutletChildProcess,
+        protected application: Application,
+    ) {
+        this.setupProcess();
+    }
 
-    abstract update(): void;
+    start(): void {
+        this.processStatus = ProcessStatus.RUNNING;
 
-    abstract close(): void;
+        if (this.command && this.arguments) {
+            const process = this.childProcess.spawn(
+                this.command,
+                this.arguments,
+            );
 
-    abstract getApplicationId(): string;
+            process.stdout.on('data', (data: Buffer) => {
+                const output = data.toString();
+                this.stdout.push(output);
+            });
 
-    abstract getProcessStatus(): ProcessStatus;
+            process.on('error', (data: Buffer) => {
+                console.error(`ps stderr: ${data.toString()}`);
+                this.stderr.push(data.toString());
+            });
 
-    abstract getProcessInfo(): ProcessInfo;
+            process.stderr.on('data', (data: Buffer) => {
+                console.error(`ps stderr: ${data.toString()}`);
+                this.stderr.push(data.toString());
+            });
+
+            process.on('close', () => {
+                this.processStatus = ProcessStatus.FINISHED;
+                this.onProcessFinished(this);
+            });
+        }
+    }
+
+    protected setCommand(command: string, args: string[]): void {
+        this.command = command;
+        this.arguments = args;
+    }
+
+    abstract setupProcess(): void;
+
+    getApplicationId(): string {
+        return this.application.id;
+    }
+
+    getProcessStatus(): ProcessStatus {
+        return this.processStatus;
+    }
+
+    getProcessInfo(): ProcessInfo {
+        return {
+            applicationId: this.getApplicationId(),
+            processStatus: this.getProcessStatus(),
+        };
+    }
+
+    abstract onProcessFinished(process: Process): void;
 }
 
 export enum ProcessStatus {
--- src/app/service/process/snap/uninstall-snap.process.ts
+++ src/app/service/process/snap/uninstall-snap.process.ts
@@ -1,63 +1,27 @@
-import { Process, ProcessStatus } from '../process';
-import { ProcessInfo } from '../process-info';
+import { Process } from '../process';
 import { AppOutletChildProcess } from '../../../util/app-outlet-child-process';
 import { Application } from '../../../model/application.model';
 
 export class UninstallSnap extends Process {
-    private processStatus = ProcessStatus.IDLE;
-
     constructor(
-        private childProcess: AppOutletChildProcess,
-        private application: Application,
-        private onProcessFinished: (process: Process) => void,
+        childProcess: AppOutletChildProcess,
+        application: Application,
+        private onProcessFinishedCallback: (process: Process) => void,
     ) {
-        super();
+        super(childProcess, application);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    close(): void {}
-
-    getApplicationId(): string {
-        return this.application.id;
-    }
-
-    getProcessInfo(): ProcessInfo {
-        return {
-            applicationId: this.getApplicationId(),
-            processStatus: this.processStatus,
-        };
-    }
-
-    getProcessStatus(): ProcessStatus {
-        return this.processStatus;
-    }
-
-    start(): void {
-        this.processStatus = ProcessStatus.RUNNING;
-
+    setupProcess(): void {
         const command: string[] = [
             'snap',
             'remove',
             `${this.application.packageName}`,
         ];
-        const process = this.childProcess.spawn('pkexec', command);
 
-        process.on('error', (data: Buffer) => {
-            console.error(`ps stderr: ${data.toString()}`);
-            this.stderr.push(data.toString());
-        });
-
-        process.stderr.on('data', (data: Buffer) => {
-            console.error(`ps stderr: ${data.toString()}`);
-            this.stderr.push(data.toString());
-        });
-
-        process.on('close', () => {
-            this.processStatus = ProcessStatus.FINISHED;
-            this.onProcessFinished(this);
-        });
+        this.setCommand('pkexec', command);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    update(): void {}
+    onProcessFinished(process: Process): void {
+        this.onProcessFinishedCallback?.(process);
+    }
 }
