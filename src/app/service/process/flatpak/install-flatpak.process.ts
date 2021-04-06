d046168c0400f827e391a1985ed3e2d39ad48891
Messias Junior <messiaslima.03@gmail.com>
2021-04-06 23:40:31 +0100

    feat: calculate flatpak complete percentage

---
+++ src/app/service/process/flatpak/install-flatpak.process.ts
@@ -0,0 +1,80 @@
+import { Process } from '../process';
+import { AppOutletChildProcess } from '../../../util/app-outlet-child-process';
+import { Application } from '../../../model/application.model';
+
+export class InstallFlatpak extends Process {
+    constructor(
+        childProcess: AppOutletChildProcess,
+        application: Application,
+        private onProcessFinishedCallback: (process: Process) => void,
+    ) {
+        super(childProcess, application);
+        this.isIndefinite = true;
+    }
+
+    onProcessFinished(process: Process): void {
+        this.onProcessFinishedCallback(process);
+    }
+
+    setupProcess(): void {
+        this.setCommand('flatpak', [
+            'install',
+            'flathub',
+            this.application.id,
+            '-y',
+        ]);
+    }
+
+    protected onUpdate(update: string): void {
+        const outputFragment = update.split(' ');
+        const steps = this.getNumberSteps(outputFragment);
+        const installationPercent = this.getPercentage(outputFragment);
+
+        if (!installationPercent) {
+            return;
+        }
+
+        const base =
+            (steps.currentStep / steps.maxStep - 1 / steps.maxStep) * 100;
+        const currentStepPercentage = installationPercent / steps.maxStep;
+
+        this.completePercentage = base + currentStepPercentage;
+    }
+
+    private getNumberSteps(outputFragment: string[]): Steps {
+        const stepsRegex = /\d+\/\d+…/;
+        const fragment = outputFragment.find((outputItem) => {
+            return stepsRegex.test(outputItem);
+        });
+        if (!fragment) {
+            return {
+                currentStep: 1,
+                maxStep: 1,
+            };
+        }
+
+        const fragmentArray = fragment.replace('…', '').split('/');
+        return {
+            currentStep: Number(fragmentArray[0]),
+            maxStep: Number(fragmentArray[1]),
+        };
+    }
+
+    private getPercentage(outputFragment: string[]): number | null {
+        const stepsRegex = new RegExp('\\d+%');
+        const fragment = outputFragment.find((outputItem) =>
+            stepsRegex.test(outputItem),
+        );
+
+        if (!fragment) {
+            return null;
+        }
+
+        return Number(fragment.replace('%', ''));
+    }
+}
+
+interface Steps {
+    currentStep: number;
+    maxStep: number;
+}
