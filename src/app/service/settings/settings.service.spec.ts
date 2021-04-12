3c2f324209c854618e66ee2c410e5e5549e5c92e
Messias Junior <messiaslima.03@gmail.com>
2021-04-12 22:35:08 +0100

    test: settings service

--- src/app/service/settings/settings.service.spec.ts
+++ src/app/service/settings/settings.service.spec.ts
@@ -2,10 +2,13 @@ import { TestBed } from '@angular/core/testing';
 
 import { SettingsService } from './settings.service';
 import { CoreService } from '../core/core.service';
+import { SettingModel } from '../../model/setting.model';
 
 describe('SettingsService', () => {
     let service: SettingsService;
-    const mockCoreService = {};
+    const mockCoreService = {
+        invoke: jest.fn(),
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -14,7 +17,34 @@ describe('SettingsService', () => {
         service = TestBed.inject(SettingsService);
     });
 
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should get theme', async () => {
+        const setting: SettingModel = {
+            id: 1,
+            key: 'theme',
+            value: 'cosmic',
+        };
+
+        mockCoreService.invoke.mockReturnValue(Promise.resolve(setting));
+        const theme = await service.getTheme();
+        expect(theme).toEqual(setting.value);
+
+        mockCoreService.invoke.mockReturnValue(Promise.resolve(undefined));
+        const nullTheme = await service.getTheme();
+        expect(nullTheme).toBeUndefined();
+    });
+
+    it('should set theme', async () => {
+        const theme = 'cosmic';
+        await service.setTheme(theme);
+
+        expect(mockCoreService.invoke.mock.calls[0][1]).toEqual(theme);
     });
 });
