ca625f9f3251f7963fe258d9f0113c1cc6b4e793
Messias Junior <messiaslima.03@gmail.com>
2022-03-01 21:26:32 +0000

    test: markdown pipe

--- src/app/util/markdown-pipe/markdown.pipe.spec.ts
+++ src/app/util/markdown-pipe/markdown.pipe.spec.ts
@@ -1,8 +1,22 @@
 import { MarkdownPipe } from './markdown.pipe';
 
 describe('MarkdownPipe', () => {
+    const pipe = new MarkdownPipe();
     it('create an instance', () => {
-        const pipe = new MarkdownPipe();
         expect(pipe).toBeTruthy();
+    });
+
+    it('should return undefined if we pass undefined', () => {
+        expect(pipe.transform(undefined)).toBeUndefined();
+    });
+
+    it('should return empty string if we pass empty string', () => {
+        expect(pipe.transform('')).toEqual('');
+    });
+
+    it('should transform markdown to html', () => {
+        expect(pipe.transform('# app-outlet')).toEqual(
+            '<h1 id="app-outlet">app-outlet</h1>\n',
+        );
     });
 });
